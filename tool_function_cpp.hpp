534c647592cb8c3660f8d0bfb6cb099ca9bccf13
Kuan-Lin <sony.zero@hotmail.com>
2020-01-16 15:22:56 +0900

    Add matrix function.

--- tool_function_cpp.hpp
+++ tool_function_cpp.hpp
@@ -5,3 +5,26 @@ inline double DotProduct( double *A, double *B )
 	return A[0]*B[0] + A[1]*B[1] + A[2]*B[2] ;
 };
 
+// double ** MatAlloc( int row, int col )
+// {
+// 	double ** ptr = new double*[row];
+// 	for( int i=0 ; i<row ; i++)
+// 	{
+// 		ptr[i] = new double[col];
+// 	}
+
+// 	for (int i=0 ; i < row ; i++ ){
+// 		for (int j=0 ; j < col ; j++ ){
+// 			ptr[i][j]=0.0;
+// 		}
+// 	}
+// 	return ptr;
+// }
+// void MatFree ( double** ptr, int row, int col)
+// {
+// 	for(int i = 0; i < row; i++)
+// 	{
+// 		delete [] ptr[i];
+// 	}
+// 	delete [] ptr;
+// }
